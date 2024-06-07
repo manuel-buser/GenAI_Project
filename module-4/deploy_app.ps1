07766d38d5a7b437daa55206f5e66790ec12b3fd
Schiiss <conner.schiissler@gmail.com>
2024-06-07 17:35:09 -0600

    adding prep docs notebook and added -update flag to script to deploy zip file to app service

--- module-4/deploy_app.ps1
+++ module-4/deploy_app.ps1
@@ -1,3 +1,3 @@
-Compress-Archive -Path ..\module-3\Home.py, ..\module-3\requirements.txt -DestinationPath .\module-4
+Compress-Archive -Path ..\module-3\Home.py, ..\module-3\requirements.txt -DestinationPath .\module-4 -Update
 
 az webapp deploy --resource-group "genai-on-azure" --name "genai-on-azure-chatbot-app-service" --src-path .\module-4.zip
